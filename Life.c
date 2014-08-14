b5998403100821a874d49c257b1d1cf7d438d92e
Aftix <gameraftexploision@gmail.com>
2014-08-14 13:58:53 -0500

    Fixed arrow keys

--- Life.c
+++ Life.c
@@ -13,7 +13,7 @@ void sim(Cell *grid);
 int main() {
 	initscr();
 	erase();
-	keypad(stdscr, 1);
+	keypad(stdscr, TRUE);
 	noecho();
 	cbreak();
 	curs_set(0);
@@ -42,14 +42,14 @@ void init(Cell *grid) {
 		
 	timeout(10);
 	while (1) {
-		char ch = getch();
+		int ch = getch();
 		
 		if (ch == 'q') return;
 		
-		if (ch == 'w') y--;
-		if (ch == 's') y++;
-		if (ch == 'a') x--;
-		if (ch == 'd') x++;
+		if (ch == 'w' || ch == KEY_UP) y--;
+		if (ch == 's' || ch == KEY_DOWN) y++;
+		if (ch == 'a' || ch == KEY_LEFT) x--;
+		if (ch == 'd' || ch == KEY_RIGHT) x++;
 
 		if (y > LINES-1) y = LINES-1;
 		if (x > COLS-1) x = COLS-1;
@@ -76,7 +76,7 @@ void sim(Cell *grid) {
 	curs_set(0);
 
 	timeout(-1);
-	char ch;
+	int ch;
 	Cell *next = malloc(LINES*COLS*sizeof(Cell));
 
 	while (1) {
@@ -93,7 +93,7 @@ void sim(Cell *grid) {
 				if (j > 0) count += grid[i*LINES + j - 1].state;
 				if (j < COLS-1) count += grid[i*LINES + j + 1].state;
 				if (i > 0) count += grid[(i-1)*LINES + j].state;
-				if (i < LINES-1) count += grid[(i+1)*LINES + j]. state;
+				if (i < LINES-1) count += grid[(i+1)*LINES + j].state;
 				if (j > 0 && i > 0) count += grid[(i-1)*LINES + j - 1].state;
 				if (j > 0 && i < LINES-1) count += grid[(i+1)*LINES + j - 1].state;
 				if (j < COLS-1 && i > 0) count += grid[(i-1)*LINES + j + 1].state;
